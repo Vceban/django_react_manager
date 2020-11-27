cf2f1e53525d784c08e7db98f8ef0798810934bb
Vceban <awan2rist@gmail.com>
2020-11-27 23:44:09 +0200

    test commit

---
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class Form extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1> Add Lead Form </h1>	
+			</div>
+		)
+	}
+}
+
+export default Form	
