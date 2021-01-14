fad220b8979d73e67ebc9570ea205d7da86b49df
Vceban <awan2rist@gmail.com>
2021-01-14 23:04:49 +0200

    added login and register page

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,9 +1,12 @@
 import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
+import{ HashRouter as Router, Route, Switch, Redirect} from "react-router-dom"
 
 import Header from './layout/Header';
-import Dashbord from './leads/Dashbord';
 
+import Dashbord from './leads/Dashbord';
+import Login from "./accounts/Login"
+import Register from "./accounts/Register"
 import { Provider } from 'react-redux';
 import store from '../store';
 
@@ -11,12 +14,19 @@ class App extends Component {
 	render() {
 		return (
 			<Provider store={store}>
+			<Router>
 			 <Fragment>
 			  <Header />
 			  <div className="container">
-				<Dashbord />
+			  <Switch>
+			  <Route exact path ="/" component={Dashbord} />
+			   <Route exact path ="/" component={Register} />
+			    <Route exact path ="/" component={Login} />
+				
+				</Switch>
 			  </div>
 			 </Fragment>
+			 </Router>
 			</Provider>
 			);
 	}
